4225370ea39e45f16cb54870a774611b6c9a42dd
Isaac Oh <isaacohishere@gmail.com>
2018-05-01 00:17:16 -0700

    more fixes

--- src/app/layout/Footer.js
+++ src/app/layout/Footer.js
@@ -1,17 +1,38 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => (
   <footer className="footer">
     <div className="container">
       <div className="content has-text-centered">
-        <p>
-          Hayoung Yoo
-          <br />
-          Copyright 2018
-        </p>
+        <p>Hayoung Yoo</p>
+        <div className="icon-buttons">
+          <a className="icon-button mail" href="mailto:Hayoung.Yoo@svusd.org">
+            <i className="fas fa-envelope icon-mail" />
+            <span />
+          </a>
+          <a
+            className="icon-button linkedin"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.linkedin.com/in/hayoung-yoo-b5071375/"
+          >
+            <i className="fab fa-linkedin icon-linkedin" />
+            <span />
+          </a>
+          <a
+            className="icon-button twitter"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/TeachMsYoo"
+          >
+            <i className="fab fa-twitter icon-twitter" />
+            <span />
+          </a>
+        </div>
+        <p>Copyright 2018</p>
       </div>
     </div>
   </footer>
-)
+);
 
-export default Footer
+export default Footer;
